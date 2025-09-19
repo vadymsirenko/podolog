8d19c87ddbaec4902317f78290b01b1006cf9a69
Anna <annadacenkomail@gmail.com>
2025-09-19 12:57:35 +0200

    faq is finish

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ nav?.querySelectorAll("a").forEach((a) =>
   document.querySelectorAll(".faq-item").forEach(item => {
     item.addEventListener("click", () => {
       const p = item.querySelector("p");
+      item.classList.toggle("active");
       p.style.display = (p.style.display === "block") ? "none" : "block";
     });
   });
